d7623f9b282d555c72874ab54fe6cb6ae5c7c86b
Chimezie Okeke <ChimezieOkeke@wmpenn.edu>
2022-02-04 01:13:30 -0600

    Firebase Library

--- ahcer/src/environments/environment.prod.ts
+++ ahcer/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'wpu-ahcer',
+    appId: '1:503871560040:web:b9d5945d8ece53d2241edd',
+    storageBucket: 'wpu-ahcer.appspot.com',
+    apiKey: 'AIzaSyAnC2nU3mtFKZd12PFk1TQkXRsR4aA0JL8',
+    authDomain: 'wpu-ahcer.firebaseapp.com',
+    messagingSenderId: '503871560040',
+  },
   production: true
 };
