028bb8f9ab33a745ec196aa4292c9d34770b7c52
Chimezie Okeke <ChimezieOkeke@wmpenn.edu>
2022-02-07 23:20:24 -0600

    New Firebase

--- ahcer/src/environments/environment.ts
+++ ahcer/src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
     projectId: 'wpu-ahcer',
     appId: '1:503871560040:web:b9d5945d8ece53d2241edd',
     storageBucket: 'wpu-ahcer.appspot.com',
+    locationId: 'us-central',
     apiKey: 'AIzaSyAnC2nU3mtFKZd12PFk1TQkXRsR4aA0JL8',
     authDomain: 'wpu-ahcer.firebaseapp.com',
     messagingSenderId: '503871560040',
