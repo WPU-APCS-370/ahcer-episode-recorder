6ebbbc2aa48de16fabfa86f344527fd8399fa53b
Arsalan Javed <Arsal024@gmail.com>
2024-05-13 16:25:45 +0500

    changed from mobile app to web

---
+++ ahcer/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { Validators, UntypedFormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
+import { first, switchMap } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent {
+  public signUpError: string = '';
+
+  userForm =this.fb.group({
+    username: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  constructor(
+    private fb: UntypedFormBuilder,
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private userService: UsersService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  async onCreateUser() {
+    const val = this.userForm.value;
+
+    this.afAuth.app.then(cred => {
+      let parentId = cred.auth().currentUser?.uid ? cred.auth().currentUser?.uid : '';
+      this.userService
+        .createUserByEmailPassword(val.email, val.password).then((res:any)=>{
+          const childId = res.user.uid;
+            this.db.firestore.doc(`users/${childId}`)
+              .get()
+              .then((doc)=> {
+                const body = {
+                  isParent: parentId ? false : true,
+                  username:val.username, 
+                  email: val.email,
+                  password:val.password
+                }
+                if (parentId) {
+                  body['parentId'] =  parentId;
+                }
+                return !doc.exists ?
+                  this.db.collection(`users`).doc(childId).set(body) :
+                  null;
+              });
+            if (parentId) {
+              this.db.firestore.doc(`users/${parentId}`)
+              .get()
+              .then((doc)=> {
+                if (doc.exists) {
+                  const user = doc.data();
+                  user.child ? user.child.push(childId) : user.child = [childId];
+                  this.db.collection(`users`).doc(parentId).update({child:user.child, isParent: true})
+                }
+              });
+            }
+            this.router.navigateByUrl('/users')
+        }).catch((error)=>{
+          this.signUpError = error;
+          setTimeout(() => {
+            this.signUpError = '';
+          }, 5000);
+            console.log(error);
+        })
+    });
+
+  }
+}
---
+++ ahcer/src/app/view-video/view-video.component.ts
@@ -0,0 +1,155 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Patient } from '../models/patient';
+import { Medication } from '../models/medication';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { PatientServices } from '../services/patient.service';
+import { MedicationService } from '../services/medication.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs/operators';
+import { EditPatientComponent } from '../edit-patient/edit-patient.component';
+import { DeletePatientComponent } from '../delete-patient/delete-patient.component';
+import { UsersService } from '../services/users.service';
+import { error } from 'console';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-view-video',
+  templateUrl: './view-video.component.html',
+  styleUrls: ['./view-video.component.scss']
+})
+export class ViewVideoComponent {
+  @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement>;
+  videos: any[] = []
+  loading: boolean = false;
+  public fileUploading: boolean = false;
+  public fileUploadMessage: string = '';
+  public fileUploadError: string = '';
+
+  constructor(private dialog: MatDialog,
+              private patientService: PatientServices,
+              private userService: UsersService,
+              private storage: AngularFireStorage
+            ) { }
+
+  ngOnInit(): void {
+    this.loadVideos();
+  }
+
+  loadVideos() {
+    this.loading = true;
+
+    this.userService.getUserVideos()
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe(
+        (result) => {
+          if (result.videos) {
+            this.videos= result.videos;
+            if (result.videos.length > 0) {
+              this.videos = result.videos.sort().reverse()  ?? [];
+            }
+          }else{
+            this.videos = [];
+          }
+        }
+      )
+  }
+
+  deleteFile(filePath: string): Observable<any> {
+    const fileRef = this.storage.refFromURL(filePath);
+    return fileRef.delete();
+  }
+
+  deleteVideoFromUser(videoIndex: number){
+    if (videoIndex !== -1) {
+      this.videos.splice(videoIndex, 1);
+    }
+    this.userService.updateUserVideoArray(this.videos).subscribe(()=>{
+      this.fileUploadMessage = 'Video Deleted Successfully'
+      // this.loadVideos();
+    }, (error)=>{
+      this.setFileUploadError('Some error occured');
+    })
+  }
+
+  onDeleteVideo(videoIndex: number, videoLink:string) {
+    const filePath = videoLink;
+    this.deleteFile(filePath).subscribe(
+      () => {
+        this.fileUploadMessage = 'File deleted successfully';
+        this.deleteVideoFromUser(videoIndex)
+      },
+      (error) => {
+        this.fileUploadError = 'Error deleting file:';
+        this.loadVideos();
+      }
+    );
+  }
+
+
+  uploadClick() {
+    this.fileInput.nativeElement.click();
+  }
+
+  uploadFile(event) {
+    this.fileUploadError='';
+    this.fileUploadMessage='';
+    this.fileUploading = true;
+
+    const file = event.target.files[0];
+    
+    // Check if the selected file is a video
+    if (file.type.startsWith('video/')) {
+      const filePath = 'videos/' + file.name;
+      const fileRef = this.storage.ref(filePath);
+      const task = this.storage.upload(filePath, file);
+
+      // Get the upload progress percentage
+      task.percentageChanges().subscribe(percentage => {
+        console.log('Upload is ' + percentage + '% done');
+        this.fileUploadMessage = 'Video ' + Math.floor(percentage) + '% uploaded';
+      });
+
+      // Get the download URL when the upload is completed
+      task.snapshotChanges().pipe(
+        finalize(() => {
+          fileRef.getDownloadURL().subscribe(downloadURL => {
+            console.log('Video available at', downloadURL);
+            this.fileUploadMessage = 'Video Uploaded Successfully!'
+            const randomId = Math.floor(100000 + Math.random() * 900000).toString();
+            const list = this.videos;
+            list.push({
+              name: file.name,
+              link:  downloadURL,
+              id: randomId
+            });
+            this.videos = this.videos.sort().reverse();
+            this.userService.updateUserVideoArray(list).subscribe(()=>{
+              this.fileUploadMessage = 'Video Uploaded Successfully'
+            }, (error)=>{
+              console.log(error);
+              this.setFileUploadError('Some error occured');
+            })
+          });
+
+          setTimeout(() => {
+            this.fileUploading = false;
+          }, 3000);
+        })
+      ).subscribe();
+    } else {
+      this.fileUploading= false;
+      this.setFileUploadError('Invalid file format. Please select a video file.');
+    }
+  }
+
+  setFileUploadError(error:string){
+    this.fileUploadError = error;
+      setTimeout(() => {
+        this.fileUploadError=''
+      }, 5000);
+  }
+}
